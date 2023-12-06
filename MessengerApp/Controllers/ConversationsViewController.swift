7b2796cd57ed581ff7ffd5559f0efbc2f9f8c6cd
Smruthi Pobbathi <smruthi096@gmail.com>
2023-12-05 23:37:22 -0800

    Integrated Firebase and added login and registration

---
+++ MessengerApp/Controllers/ConversationsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ConversationsViewController.swift
+//  MessengerApp
+//
+//  Created by Smruthi Pobbathi on 12/5/23.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+    
+
+}
+
---
+++ MessengerApp/Resources/Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  Extensions.swift
+//  MessengerApp
+//
+//  Created by Smruthi Pobbathi on 12/5/23.
+//
+
+import UIKit
+import Foundation
+
+extension UIView {
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
+
